21a398324b1cabb53186c6dd8c4b8407ea5a17ef
Your Name <you@example.com>
2017-08-29 20:46:13 +0300

    Add delete script

--- Playground/mongodb-connect.js
+++ Playground/mongodb-connect.js
@@ -21,43 +21,42 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   // Коллекция TodoApp будет создана автоматически(если она еще не существует) как только в нее будут
   // добавлены даные.
 
-  db.collection('Todos').insertOne({
-    text: 'Something to do',
-    completed: false
-  }, (err, result) => {
-    if (err) {
-      return console.log('Unable to insert todo', err);
-    }
-    // result.ops - документы, которые были добавлены
-    console.log(JSON.stringify(result.ops, undefined, 2));
-  });
-
+  // db.collection('Todos').insertOne({
+  //   text: 'Something to do',
+  //   completed: false
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert todo', err);
+  //   }
+  //   // result.ops - документы, которые были добавлены
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  // });
 
   //
-  db.collection('Users').insertOne({
-    name: 'Andrew',
-    age: 25,
-    location: 'SPB'
-  }, (err, result) => {
-    if (err) {
-      return console.log('Unable to insert todo', err);
-    }
-    // result.ops - документы, которые были добавлены
-    // Документу автоматически будет присвоен случаный 20байтный _id в виде хеша.
-    // 4 байта (Timestamp) - время создания объекта, затем 3 - идентификатор машины, 2 - идентификатор
-    // процесса, 3 - счетчик(случайное число).
-    // Это удобно, когда нужно добавить дополнительный сервер, нет
-    // нобходимости в обращению к другим серверам чтобы узнать
-    // максимальный id и инкрементировать его, как это могло
-    // быть с SQL базой. _id можно указать и вручную. MongoDB
-    // используется в качетве _id не число, ObjectID.
-    // let obj = new ObjectID();
-    // console.log(obj);
-
-    console.log(JSON.stringify(result.ops, undefined, 2));
-
-    console.log(result.ops[0]._id.getTimestamp());
-  });
+  // db.collection('Users').insertOne({
+  //   name: 'Andrew',
+  //   age: 25,
+  //   location: 'SPB'
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert todo', err);
+  //   }
+  //   // result.ops - документы, которые были добавлены
+  //   // Документу автоматически будет присвоен случаный 20байтный _id в виде хеша.
+  //   // 4 байта (Timestamp) - время создания объекта, затем 3 - идентификатор машины, 2 - идентификатор
+  //   // процесса, 3 - счетчик(случайное число).
+  //   // Это удобно, когда нужно добавить дополнительный сервер, нет
+  //   // нобходимости в обращению к другим серверам чтобы узнать
+  //   // максимальный id и инкрементировать его, как это могло
+  //   // быть с SQL базой. _id можно указать и вручную. MongoDB
+  //   // используется в качетве _id не число, ObjectID.
+  //   // let obj = new ObjectID();
+  //   // console.log(obj);
+  //
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  //
+  //   console.log(result.ops[0]._id.getTimestamp());
+  // });
 
   db.close();
 });
